c13bf241ace3461db1b242ae960f20991c0a894f
Matīss Rikters <matiss_rikters@hotmail.com>
2022-02-16 11:02:30 +0900

    Iestatījumu tulkojumi

--- src/components/modals/SettingsModal.tsx
+++ src/components/modals/SettingsModal.tsx
@@ -27,20 +27,20 @@ export const SettingsModal = ({
   handleHighContrastMode,
 }: Props) => {
   return (
-    <BaseModal title="Settings" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="Iestatījumi" isOpen={isOpen} handleClose={handleClose}>
       <div className="grid-cols-2 gap-4">
         <SettingsToggle
-          settingName="Hard Mode"
+          settingName="Grūtais režīms"
           flag={isHardMode}
           handleFlag={handleHardMode}
         />
         <SettingsToggle
-          settingName="Dark Mode"
+          settingName="Tumšais režīms"
           flag={isDarkMode}
           handleFlag={handleDarkMode}
         />
         <SettingsToggle
-          settingName="High Contrast Mode"
+          settingName="Augsta kontrasta režīms"
           flag={isHighContrastMode}
           handleFlag={handleHighContrastMode}
         />
