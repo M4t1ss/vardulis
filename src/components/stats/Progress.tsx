f93511944fb181f16620241c55e435fdcd9febfa
Matīss Rikters <matiss_rikters@hotmail.com>
2022-03-25 13:40:09 +0900

    Progress styling

--- src/components/stats/Progress.tsx
+++ src/components/stats/Progress.tsx
@@ -9,13 +9,13 @@ type Props = {
 
 export const Progress = ({ index, size, label, currentDayStatRow }: Props) => {
   const currentRowClass = classNames(
-    'text-xs font-medium text-blue-100 text-center p-0.5',
-    { 'bg-blue-600': currentDayStatRow, 'bg-gray-600': !currentDayStatRow }
+    'text-xs font-medium text-blue-100 text-center p-0.5 rounded-full',
+    { 'bg-blue-600': currentDayStatRow, 'bg-gray-500': !currentDayStatRow }
   )
   return (
     <div className="flex justify-left m-1">
       <div className="items-center justify-center w-2">{index + 1}</div>
-      <div className="w-full ml-2">
+      <div className="bg-gray-200 w-full ml-2 rounded-full">
         <div style={{ width: `${8 + size}%` }} className={currentRowClass}>
           {label}
         </div>
